90414f3d94254bd8abb674f982f2e94a3db219ad
Erik Basargin <basargin.erik@gmail.com>
2025-02-12 22:20:08 +0000

    Update Capture module supported platforms list

--- ODCLite/FeatureModules/Capture/Package.swift
+++ ODCLite/FeatureModules/Capture/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Capture",
+    platforms: [
+        .macOS(.v15),
+    ],
     products: [
         .library(
             name: "Capture",
