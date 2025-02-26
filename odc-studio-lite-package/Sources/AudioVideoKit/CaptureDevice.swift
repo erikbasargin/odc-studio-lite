5683d84e5b507b7b3b29a49e3671d107b869f127
Erik Basargin <basargin.erik@gmail.com>
2025-02-26 18:39:05 +0000

    Remove CaptureDevice/init deprecation

--- odc-studio-lite-package/Sources/AudioVideoKit/CaptureDevice.swift
+++ odc-studio-lite-package/Sources/AudioVideoKit/CaptureDevice.swift
@@ -7,7 +7,6 @@ public struct CaptureDevice: Identifiable, Hashable, Sendable {
     public let id: String
     public let name: String
     
-    @available(*, deprecated, message: "Will be removed")
     public init(id: String, name: String) {
         self.id = id
         self.name = name
