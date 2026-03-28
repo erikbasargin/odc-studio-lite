9ef43ac32ab17e2b7ae9b3d035b3bd6fb02b7428
Erik Basargin <basargin.erik@gmail.com>
2026-03-28 08:47:50 +0000

    Connect with Tuist Cloud

--- Tuist.swift
+++ Tuist.swift
@@ -1,7 +1,11 @@
 import ProjectDescription
 
 let tuist = Tuist(
+    fullHandle: "erik-basargin/odc-studio-lite",
     project: .tuist(
         swiftVersion: "6.3",
-    )
+        generationOptions: .options(
+            enableCaching: Environment.enableCaching.getBoolean(default: false),
+        ),
+    ),
 )
